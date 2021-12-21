b5292cb24735ba532849c406e1dec96568b4a64b
jinoung <jinoung@gmail.com>
2021-12-22 01:03:58 +0900

    delete useless files

--- projects/myproject/myproject.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd c:\Flask\projects\myproject
-set FLASK_APP=pybo
-set FLASK_ENV=development
-c:\venvs\myproject\Scripts\activate
--- projects/myproject1/myproject1.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd c:\Flask\projects\myproject1
-set FLASK_APP=bbs
-set FLASK_ENV=development
-c:\venvs\myproject1\Scripts\activate
